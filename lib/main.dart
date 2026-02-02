1d63a2d61ee729dc393c9de975fc4a3d47876f67
chana <chanapoljaimun@gmail.com>
2026-02-02 09:59:35 +0700

    ver.0

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+//import 'teamproject/best.dart';
+//import 'teamproject/oat.dart';
+//import 'teamproject/pom.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Team Showcase")),
+      body: SingleChildScrollView(
+        child: Center(
+          //        child: Column(children: const [Member1(), Member2(), Member3()]),
+        ),
+      ),
+    );
+  }
+}
